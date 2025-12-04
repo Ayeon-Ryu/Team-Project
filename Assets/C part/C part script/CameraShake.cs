b1bf263dee345919533aed8da4ed2c14e686cc69
arark2 <everydaywhite4002@gmail.com>
2025-12-04 12:15:14 +0900

    카메라 흔들림(멀미 좀 심하게)

--- Assets/C part/C part script/CameraShake.cs
+++ Assets/C part/C part script/CameraShake.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using System.Collections;
 
 public class CameraShake : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float intensity = 0.1f;   // 흔들림 강도
+    public float duration = 0.5f;    // 흔들림 지속 시간
+
+    private Vector3 originalPos;
+
     void Start()
     {
-        
+        originalPos = transform.localPosition;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayShake()
     {
-        
+        StartCoroutine(ShakeCoroutine());
+    }
+
+    IEnumerator ShakeCoroutine()
+    {
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * intensity;
+            float y = Random.Range(-1f, 1f) * intensity;
+
+            transform.localPosition = originalPos + new Vector3(x, y, 0f);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localPosition = originalPos;
     }
 }
